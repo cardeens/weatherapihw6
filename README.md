d922566df742b54efcfa066bf60bf0e86c5ccbbb
JJ Cardenas <jjcardenas16@hotmail.com>
2020-03-24 22:32:48 -0700

    updates to JS and HTML

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # weatherapihw6
+
+# Description
+
+This deliverable is a simple and easy to use weather tracker that can give us the current weather and forecast in any given city. Searching for a city will give the user the current stats, and a five day forecast of the weather coming up in the future, and then save the city as an existing button for future use.
+
+We completed this project by using the Open Weather public APIs. This allowed us to get real-time weather data to be able to build out our website. By getting an API key and using it to access the data point by city, we are able to generate a forecast of real-time weather information for the user. We used jQuery to attach these data points to elements in our website and create a simple UI for the user that is easily understandable. Using jQuery allowed us to use very minimal HTML.
+
+An applicaiton such as this is useful because it provides real time data from an existing source in a simple and easy to use websute. It is important for us to know how to use these APIs so that we can gain access to many different data sources.
+
+## Installation
+
+To begin using this website and reviewing the code, 
+
+## Usage
+
+## Testing
+
--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
-var btn = [];
 var key = "fb021fb2cd887810a1af82c9cd804746";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + $(city) "&id=524901&appid=" + key
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + key;
 
+$(document).on("click", ".searchBtn", function() {
+    console.log("Hello");
+    cityWeather(cityValue);
+    console.log(data)
+})
 
+function addRow(text) {
+    var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+    $(".history").append(li);
+  }
 
-$.get(queryURL)
-    .then(
-        function (data) {
-            console.log(data)
+function cityWeather(cityValue) {
+    $.get(queryURL, function(data) {
+        console.log(data)
+        if (history.indexOf(searchValue) === -1) {
+            history.push(searchValue);
+            window.localStorage.setItem("history", JSON.stringify(history));
+            addRow(searchValue);
         }
-    )
 
-    $(document).on("click, "."cityBtn", function() {
+$("#today").empty();
 
-        
-    })
+var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+var card = $("<div>").addClass("card");
+var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+var rain = $("<p>").addClass("card-text").text("Rain Forecast: " + data.rain);
+var cardBody = $("<div>").addClass("card-body");
+var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
+title.append(img);
+cardBody.append(title, temp, humid, wind);
+card.append(cardBody);
+$("#today").append(card);
+
+}
+});
